bc7ca6c2e1bf68591b41e7742ec34c272da9e18a
Vipin Thomas <vipin.thomas@stud.th-deg.de>
2021-04-13 11:38:27 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from setuptools import setup
 
+setup(name='Cathumbnailer',
+      version='0.1',
+      description='pynq-jupyter',
+      url='https://mygit.th-deg.de/vt16684/cathumbnailer',
+      author='vipin',
+      author_email='vipin.thomas@stud.th-deg.de',
+      license='MIT',
+      packages=['cathumbnailer'],
+      install_requires=[
+          'markdown',
+      ],
+      zip_safe=False)
